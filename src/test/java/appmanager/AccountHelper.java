77643b64ae2e362c07fa1bd98b20eb46399658fd
ysidorin <yuri.sidorin@cbsinteractive.com>
2018-07-06 18:40:58 +0300

    acc test

--- src/test/java/appmanager/AccountHelper.java
+++ src/test/java/appmanager/AccountHelper.java
@@ -1,15 +1,21 @@
 package appmanager;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+import static jdk.nashorn.internal.objects.NativeString.trim;
+
 public class AccountHelper extends BaseHelper {
     public static final String companyName = "QaCustomer" + System.currentTimeMillis();
-    public static final String companyNameCommon = "QaCustomer";
+    public static String firstName = "QaCustomer";
+    public static String lastName = "QaCustomer";
     public static final String address = "666 Letnikovskaya St";
     public static final String city = "Moscow";
     public static final String zip = "115114";
+
+    private String accountType;
 
     public AccountHelper(WebDriver wd) {
         super(wd);
@@ -25,39 +31,91 @@ public class AccountHelper extends BaseHelper {
     }
 
     public void createAccount(AccountType accountType) {
+
         WebElement CreateAccount = wd.findElement(By.linkText("Create Account"));
         CreateAccount.click();
         waitForElementToLoad("#ctypec");
-        WebElement Customer = wd.findElement(By.cssSelector("#ctypec"));
         WebElement Lead = wd.findElement(By.cssSelector("#ctypel"));
         WebElement Prospect = wd.findElement(By.cssSelector("#ctypep"));
         WebElement Partner = wd.findElement(By.cssSelector("#ctypepp"));
         WebElement Vendor = wd.findElement(By.cssSelector("#ctypev"));
         WebElement Generic = wd.findElement(By.cssSelector("#ctypeg"));
-        WebElement OK = wd.findElement(By.cssSelector("save-tpl-btn"));
-        if (accountType.equals("Customer")) {
-            OK.click();
-        } else if (accountType.equals("Lead")) {
+        this.accountType = accountType.toString().toLowerCase();
+        if (this.accountType.equals("customer")) {
+        } else if (this.accountType.equals("lead")) {
             Lead.click();
-        } else if (accountType.equals("Prospect")) {
+        } else if (this.accountType.equals("Prospect")) {
             Prospect.click();
-        } else if (accountType.equals("Partner")) {
+        } else if (this.accountType.equals("Partner")) {
             Partner.click();
-        } else if (accountType.equals("Vendor")) {
+        } else if (this.accountType.equals("Vendor")) {
             Vendor.click();
-        } else if (accountType.equals("generic")) {
+        } else if (this.accountType.equals("generic")) {
             Generic.click();
 
         }
-        OK.click();
+        click((By.cssSelector("#save-tpl-btn")));
     }
 
     public void fillAccountForm() {
-        setFirstName("Qa");
-        setLastName("customers");
-        setEmail(companyName + "@email.com");
-        setContactInfo_CompanyName(companyName);
-        clickSaveButton();
+        if (this.accountType.equals("lead")) {
+            setFirstName("Qa");
+            setLastName("customers");
+            setEmail(companyName + "@email.com");
+            clickUpdateButton();
+            alertOk();
+        } else {
+            setCompanyName(companyName);
+            clickSaveButton();
+            alertOk();
+        }
+    }
+
+    public void setFirstName(String firstName) {
+        WebElement CompanyName = wd.findElement(By.cssSelector("#fname"));
+        CompanyName.clear();
+        CompanyName.sendKeys(companyName);
+
+    }
+
+    public void setLastName(String lastName) {
+        WebElement CompanyName = wd.findElement(By.cssSelector("#lname"));
+        CompanyName.clear();
+        CompanyName.sendKeys(companyName);
+    }
+
+    public void setCompanyName(String companyName) {
+        WebElement CompanyName = wd.findElement(By.cssSelector("#company"));
+        CompanyName.clear();
+        CompanyName.sendKeys(companyName);
+    }
+
+    public String getCompanyName() {
+        WebElement CompanyName = wd.findElement(By.cssSelector("#detailsnameAndNumber > div"));
+        String CompanyNameText = trim(CompanyName.toString());
+        return CompanyNameText;
+    }
+
+    public void setEmail(String companyName) {
+        WebElement CompanyName = wd.findElement(By.cssSelector("#email"));
+        CompanyName.clear();
+        CompanyName.sendKeys(companyName);
+    }
+
+    private void clickSaveButton() {
+        click(By.cssSelector("#contact-finish-btn"));
+    }
+
+    private void clickUpdateButton() {
+        click(By.cssSelector("#update-acct-btn"));
+    }
+
+    private void alertOk() {
+        Alert alert = wd.switchTo().alert();
+        String alertText = alert.getText();
+        System.out.println(alertText);
+        alert.accept();
+
     }
 
 
--- src/test/java/tests/AccountsTests.java
+++ src/test/java/tests/AccountsTests.java
@@ -1,15 +1,33 @@
 package tests;
 
 import appmanager.AccountHelper;
+import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
+import static org.testng.Assert.assertEquals;
+
 public class AccountsTests extends TestBase {
+
 
     @Test
     public void createCustomer() {
         app.account().goToAccounts();
         app.account().createAccount(AccountHelper.AccountType.Customer);
+        app.account().waitForElementToBeVisible(By.cssSelector("#company"));
         app.account().fillAccountForm();
+        app.account().waitForElementToBeVisible(By.cssSelector("#moduledata-nameAndNumber"));
+        assertEquals(AccountHelper.companyName, app.account().getCompanyName());
+//        assert(Customer, AccountsSearch.Customer);
+
+    }
+
+    @Test
+    public void createLead() {
+        app.account().goToAccounts();
+        app.account().createAccount(AccountHelper.AccountType.Lead);
+        app.account().waitForElementToBeVisible(By.cssSelector("#lname"));
+        app.account().fillAccountForm();
+
 //        assertTrue(recentAccountsTab.hasCompany(companyName));
 
     }
